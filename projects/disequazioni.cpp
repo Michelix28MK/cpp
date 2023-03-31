f772a80d831d203aeb952b04e930c8618e77ea38
Michelix28MK <michele.folci04@gmail.com>
2023-03-31 15:13:37 +0200

    formattazioine testo

--- projects/disequazioni.cpp
+++ projects/disequazioni.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-double parametro[2], incognita[1], delta;
+int parametro[2], incognita[1], delta;
 void rileva_parametri()
 {
     cout << "\n\n Inserisci a: ";
@@ -13,7 +13,9 @@ void rileva_parametri()
     cout << "\n Inserisci c: ";
     cin >> parametro[2];
 
-    cout <<endl<<endl<< parametro[0] << "x^2 + (" << parametro[1] << ")x + (" << parametro[2]<<")";
+    cout << endl
+         << endl
+         << parametro[0] << "f(x) = x^2 + (" << parametro[1] << ")x + (" << parametro[2] << ")";
 }
 void calcolo_delta()
 {
@@ -31,21 +33,25 @@ void calcolo_soluzione()
         incognita[1] = (-parametro[1] - sqrt(delta)) / (2 * parametro[0]);
     }
 }
-void disponi_risultato(){
+void disponi_risultato()
+{
     if (delta == 0)
     {
-        cout<<"X > "<<incognita[0];
-    }else if (delta < 0)
+        cout << "X > " << incognita[0];
+    }
+    else if (delta < 0)
     {
-        cout<<(parametro[0]>0)?"OGNI X":"NESSUN X";
+        cout <<"X "<< (parametro[0] > 0) ? "OGNI X" : "NESSUN X";
     }
     else
     {
-        if (incognita[0]<incognita[1])
+        if (incognita[0] < incognita[1])
         {
-            cout<<incognita[0]<<" <= X <= "<<incognita[1];
-        }else{
-            cout<<incognita[1]<<" <= X <= "<<incognita[0];
+            cout << incognita[0] << " <= X <= " << incognita[1];
+        }
+        else
+        {
+            cout << incognita[1] << " <= X <= " << incognita[0];
         }
     }
 }
@@ -59,14 +65,18 @@ int main()
     calcolo_soluzione();
     if (delta == 0)
     {
-        cout<<"\n\nEssendo che delta = 0 allora esiste una sola soluzione per l'equazione associata: \n\n X = -b/2a = "<<(-parametro[1])<<"/"<<(2 * parametro[0])<<" = "<<incognita[0];
-    }else if (delta < 0 )
+        cout << "\n\nEssendo che delta = 0 allora esiste una sola soluzione per l'equazione associata: \n\n X = -b/2a = " << (-parametro[1]) << "/" << (2 * parametro[0]) << " = " << incognita[0];
+    }
+    else if (delta < 0)
     {
-        cout<<"\n\nEssendo che delta < 0 allora non esiste X per cui f(x) = 0";
+        cout << "\n\nEssendo che delta < 0 allora non esiste X per cui f(x) = 0";
     }
-    else{
-        cout<<"\n\nEssendo che delta != 0 allora esistono due soluzioni per l'equazione associata: \n\n X1,x2 = -b + delta^(1/2) / 2a, -b - delta^(1/2) /2a => \n\n"<<(-parametro[1])<<" + "<<sqrt(delta)<<" / "<<(2 * parametro[0])<<" = "<<incognita[0]<<"\n\n"<<(-parametro[1])<<" - "<<sqrt(delta)<<" / "<<(2 * parametro[0])<<" = "<<incognita[1];
+    else
+    {
+        cout << "\n\nEssendo che delta > 0 allora esistono due soluzioni per l'equazione associata: \n\n X1,x2 = -b + delta^(1/2) / 2a, -b - delta^(1/2) /2a => \n\n"
+             << (-parametro[1]) << " + " << sqrt(delta) << " / " << (2 * parametro[0]) << " = " << incognita[0] << "\n\n"
+             << (-parametro[1]) << " - " << sqrt(delta) << " / " << (2 * parametro[0]) << " = " << incognita[1];
     }
-    cout<<"\n\nOttenuto il risultato dell'equazione associata possiamo definire che f(x) >= 0 per ";
+    cout << "\n\nOttenuto il risultato dell'equazione associata possiamo definire che f(x) >= 0 per ";
     disponi_risultato();
 }
